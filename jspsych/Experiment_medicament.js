eb920287780df49e68c5c26087ce75f41a4d880f
mullerd38 <dominique.muller@univ-grenoble-alpes.fr>
2024-09-23 16:36:35 +0200

    new

--- jspsych/Experiment_medicament.js
+++ jspsych/Experiment_medicament.js
@@ -195,6 +195,7 @@ var question = {
   }
 ]
 }
+
 var slider = {
   type: jsPsychHtmlSliderResponse,
   slider_start: 1,
@@ -202,6 +203,11 @@ var slider = {
   min: 1,
   max: 50,
   step: 1,
+  labels: [
+    '1<br>A bit more likely', 
+    '25<br>Quite a bit more likely',
+    '50<br>Massively more likely'
+  ],
   stimulus: function() {
     var response = jsPsych.data.get().last().values()[0].response.Q0;
     var questionText = "";
@@ -212,7 +218,6 @@ var slider = {
     } else {
       questionText = "Erreur : réponse inattendue.";
     }
-    
     return `
     <div class="jspsych-html-slider-response-container">
       <div class="jspsych-html-slider-response-stimulus">
@@ -221,14 +226,14 @@ var slider = {
       <div class="slider-container">
         <div class="jspsych-html-slider-response-slider"></div>
         <div class="jspsych-html-slider-response-label-container">
-          <div class="jspsych-html-slider-response-label">1<br>A bit more likely</div>
-          <div class="jspsych-html-slider-response-label">25<br>Quite a bit more likely</div>
-          <div class="jspsych-html-slider-response-label">50<br>Massively more likely</div>
-        </div>
-      </div>
-    </div>`;
+          <div class="jspsych-html-slider-response-label">1<br>A tiny extent</div>
+          <div class="jspsych-html-slider-response-label">25<br>Moderate extent</div>
+          <div class="jspsych-html-slider-response-label">50<br>Huge extent</div>
+    </div>
+  </div>
+</div>`;
   },
-  slider_width: 350 // Ensure this width is appropriate for your layout
+  slider_width: 350 // Make sure the slider width is consistent here
 };
 
 var conditional_slider = {
